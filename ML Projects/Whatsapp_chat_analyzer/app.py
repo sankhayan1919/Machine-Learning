874f6bdc5d1d4d02598dc5d55655b407b416b2a6
sankhayan1919 <sankhayansadhukhan71@gmail.com>
2025-04-07 20:10:30 +0530

    features added

--- ML Projects/Whatsapp_chat_analyzer/app.py
+++ ML Projects/Whatsapp_chat_analyzer/app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import preprocessor, helper
+import matplotlib.pyplot as plt
 import emoji
 
 st.sidebar.title("Whatsapp Chat Analyzer")
@@ -18,30 +19,45 @@ if uploaded_file is not None:
     selected_user = st.sidebar.selectbox("Show analysis w.r.t", user_list)
     
     if st.sidebar.button("Show analysis"):
-        num_messages = helper.fetch_stats(selected_user,df)
+        num_messages, words, num_media_messages, links, num_emojis, num_stickers = helper.fetch_stats(selected_user,df)
 
         st.title("Top Statistics")
-        col1, col2, col3, col4, col5, col6, col7 = st.columns(7)
+        col1, col2, col3 = st.columns(3)
         with col1:
             st.header("Total Messages")
             st.title(num_messages)
         with col2:
             st.header("Total Words")
-            st.title(df['message'].str.split().apply(len).sum())
+            st.title(words)
         with col3:
             st.header("Total Media Shared")
-            st.title(df[df['message'] == '<Media omitted>\n'].shape[0])
+            st.title(num_media_messages)
+
+        col4, col5, col6 = st.columns(3)
         with col4:
             st.header("Total Links Shared")
-            st.title(df[df['message'].str.contains('http')].shape[0])
+            st.title(links)
         with col5:
             st.header("Total Emojis Shared")
-            total_emojis = df['message'].apply(lambda x: len([c for c in x if c in emoji.EMOJI_DATA])).sum()
-            st.title(total_emojis)
+            st.title(num_emojis)
         with col6:
             st.header("Total Stickers Shared")
-            st.title(df[df['message'].str.contains('sticker')].shape[0])
-        with col7:
-            st.header("Most active user")
-            st.title(df['user'].value_counts().index[0])
+            st.title(num_stickers)
+        
+        if selected_user == 'Overall':
+            st.title("Most Active Users")
+            x, new_df = helper.most_active_users(df)
+            fig, ax = plt.subplots()
+            col1, col2 = st.columns(2)
+            with col1:
+                ax.bar(x.index, x.values)
+                plt.xticks(rotation='vertical')
+                st.pyplot(fig)
+            with col2:
+                st.dataframe(new_df)
 
+        st.title("Wordcloud")   # wordcloud
+        wc = helper.create_wordcloud(selected_user, df)
+        fig, ax = plt.subplots()
+        ax.imshow(wc)
+        st.pyplot(fig)
--- ML Projects/Whatsapp_chat_analyzer/helper.py
+++ ML Projects/Whatsapp_chat_analyzer/helper.py
@@ -1,5 +1,35 @@
+import emoji
+from wordcloud import WordCloud, STOPWORDS
+from urlextract import URLExtract
+extract = URLExtract()
+
 def fetch_stats(selected_user,df):
-    if selected_user == 'Overall':
-        return df.shape[0]
-    else:
-        return df[df['user'] == selected_user].shape[0]
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+    num_messages = df.shape[0]
+
+    num_media_messages = df[df['message'] == '<Media omitted>\n'].shape[0]    #fetch no. of media messages
+
+    words = []
+    links=[]
+    for message in df['message']:
+        words.extend(message.split())   #fetch no. of words shared
+        links.extend(extract.find_urls(message))    #fetch no. of links shared
+        emojis = df['message'].apply(lambda x: len([c for c in x if c in emoji.EMOJI_DATA])).sum()   #fetch no. of emojis shared
+        stickers = df[df['message'].str.contains('sticker')].shape[0]    #fetch no. of stickers shared
+    
+    return num_messages, len(words), num_media_messages, len(links), emojis, stickers
+
+def most_active_users(df):
+    x = df['user'].value_counts().head(5)    #fetch top 5 active users
+    df = round((df['user'].value_counts() / df.shape[0]) * 100, 2).reset_index().rename(columns={'user': 'name', 'count': 'percent'})
+    return x, df
+
+def create_wordcloud(selected_user, df):    #wordcloud
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+    stopwords = set(STOPWORDS)
+    stopwords.update(['Media', 'omitted', 'https'])  # Add words to exclude
+    wc = WordCloud(width=500, height=500, min_font_size=10, background_color='white', stopwords=stopwords)
+    wc = wc.generate(df['message'].str.cat(sep=' '))
+    return wc
